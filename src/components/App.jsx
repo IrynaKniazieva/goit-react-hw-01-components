b1a0588a36078c9c966ee481bd37f3b0313b5e1f
IrynaKniazieva <gleb0812@gmail.com>
2022-10-21 00:51:27 +0300

    profile start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import { Profile } from 'components/Profile/Profile'
+import user from 'user.json'
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile  
+      avatar={user.avatar}
+      username = {user.username}
+      tag = {user.tag}
+      location = {user.location}
+      stats ={user.stats}
+      followers = {user.stats.followers}
+      views = {user.stats.views}
+      likes = {user.stats.likes}/>
     </div>
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,3 +1,46 @@
-export const Profile = ({ userName }) => {
-    return <p>{userName}</p>;
+import propTypes from 'prop-types'
+
+export const Profile = ({ avatar, username, tag, location, stats }) => {
+    return <div>
+    <div class="profile">
+  <div class="description">
+    <img
+      src={avatar}
+      alt="User avatar"
+      class="avatar"
+    />
+    <p class="name">{username}</p>
+    <p class="tag">@{tag}</p>
+    <p class="location">{location}</p>
+  </div>
+
+  <ul class="stats">
+    <li>
+      <span class="label">Followers</span>
+      <span class="quantity">{stats.followers}</span>
+    </li>
+    <li>
+      <span class="label">Views</span>
+      <span class="quantity">{stats.views}</span>
+    </li>
+    <li>
+      <span class="label">Likes</span>
+      <span class="quantity">{stats.likes}</span>
+    </li>
+  </ul>
+</div>
+    
+  </div>;
+};
+
+Profile.propTypes = {
+    avatar: propTypes.string.isRequired,
+    username: propTypes.string.isRequired,
+    tag: propTypes.string.isRequired,
+    location: propTypes.string.isRequired,
+    stats: {
+        followers: propTypes.number.isRequired,
+        views: propTypes.number.isRequired,
+        likes: propTypes.number.isRequired,
+    }
 }
