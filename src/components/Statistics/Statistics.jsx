069ffcd27a8be00e8d0826610cfe4b305c4acf47
IrynaKniazieva <gleb0812@gmail.com>
2022-10-22 00:08:26 +0300

    in process

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,17 +4,24 @@ export const Statistics = ({title, stats}) => {
     return <section class="statistics">
     <h2 class="title">{title}</h2>
   
-    (<ul class="stat-list">
+    <ul class="stat-list">
         {stats.map(stat => (
             <li key={stats.id}>
             <span class="label">{stat.label}</span>
-            <span class="percentage">{stat.percentage}</span>
+            <span class="percentage">{stat.percentage}%</span>
           </li>
         ))}
     </ul>
   </section>
 }
 
-// StatisticsStats.propTypes = {
-//     stats: propTypes.string.isRequired,
-// }
+Statistics.propTypes = {
+    title: propTypes.string.isRequired,
+    stats: propTypes.arrayOf(
+      propTypes.shape({
+        id: propTypes.string,
+        label: propTypes.string,
+        percentage: propTypes.number,
+      })
+    ).isRequired
+}
